bd004dd40c585b7aa38c835f5b9174e814fbeefd
Curtis Christie <103606492+curtis-christie@users.noreply.github.com>
2025-10-25 18:13:52 -0400

    json middleware, ./api/items route handler start

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,10 @@
 const express = require("express");
+const itemRoutes = require("./routes/items.js");
 
 const app = express();
+
+// middleware
+app.use(express.json()); // parses incoming requests with json data
+
+// routes
+app.use("/api/items", itemRoutes);

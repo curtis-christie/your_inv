4d89a06b521dc6190069a79415351d63dd1ca902
Curtis Christie <103606492+curtis-christie@users.noreply.github.com>
2025-11-09 20:44:23 -0500

    itemSchema update

--- backend/models/itemModel.js
+++ backend/models/itemModel.js
@@ -6,6 +6,25 @@ const itemSchema = new Schema({
   name: {
     type: String,
     required: true,
+    trim: true,
+  },
+  sku: {
+    type: Number,
+    trim: true,
+    required: true,
+    unique: true,
+    maxlength: 6,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 0,
+    default: 0,
   },
 });
 

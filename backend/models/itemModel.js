5b15b1de5ca91b005bbcb0951804b22cdcb38ee4
Curtis Christie <103606492+curtis-christie@users.noreply.github.com>
2025-11-09 20:39:48 -0500

    create routes for basic CRUD

--- backend/models/itemModel.js
+++ backend/models/itemModel.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema();
 
 const itemSchema = new Schema({
-	name: {
-		type: String,
-		required: true,
-	},
+  name: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Item", itemSchema);
--- backend/routes/items.js
+++ backend/routes/items.js
@@ -3,4 +3,19 @@ import express from "express";
 
 const router = express.Router();
 
+// get all items
+router.get("/", getAllItems);
+
+// get one item
+router.get("/:id", getOneItem);
+
+// create item
+router.post("/", createItem);
+
+// update item
+router.get("/:id", updateItem);
+
+// delete item
+router.get("/:id", deleteItem);
+
 export default router;
